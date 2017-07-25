e9e619c14e97a08c3b3c0f706b539d30c47ca74b
Tom Milligan <tom@tommilligan.net>
2017-07-25 23:12:46 +0100

    Fixed script relative references

--- deploy-bin/zip-for-upload.sh
+++ deploy-bin/zip-for-upload.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-rootFolder="../"
-
 # Zip files into archive
-zip -r "$rootFolder/deploy" "$rootFolder/*" "$rootFolder/.env.example"
+zip -r deploy * .env.example
 # Make deployment folder
-mkdir "$rootFolder/deploy"
+mkdir deploy
 # Move zip into deployment folder
-mv "$rootFolder/deploy.zip" "$rootFolder/deploy/"
+mv deploy.zip deploy/
