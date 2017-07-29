145fa3386f49b94a1b6f0639dc7baf18dbfb3863
Tom Milligan <tom@tommilligan.net>
2017-07-29 11:05:05 +0100

    Increased unit test coverage, made travis unit test only

---
+++ test/unit_services.js
@@ -0,0 +1,52 @@
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+var sinon = require('sinon');
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+
+var ner = require('ner');
+
+var services = require('../src/services');
+
+describe('unit_services', function(){
+    describe('promisification of ner', function(){
+        describe('stub success', function(){
+            var entities = {
+                DATE: [],
+                LOCATION: ['Tesocs'],
+                MONEY: [],
+                ORGANIZATION: [],
+                PERCENT: [],
+                PERSON: ['John Smith'],
+                TIME: []
+            }
+            beforeEach(function(){
+                var stubReturn = {
+                    entities: entities
+                };
+                sinon.stub(ner, 'get').yields(null, stubReturn);
+            });
+            it('extractEntities resolves simple entities', function(){
+                return expect(services.extractEntities('John Smith went to Tescos'))
+                    .to.eventually.deep.equal(entities);
+            });
+            afterEach(function() {
+                ner.get.restore();
+            });
+        });
+        describe('stub failure', function(){
+            beforeEach(function(){
+                sinon.stub(ner, 'get').yields(new Error('Generic error'), null);
+            });
+            it('extract entities rejects with the internal error', function(){
+                return expect(services.extractEntities('John Smith went to Tescos'))
+                    .to.eventually.be.rejectedWith('Could not reach NER service');
+            });
+            afterEach(function() {
+                ner.get.restore();
+            });
+        });
+    });
+});
