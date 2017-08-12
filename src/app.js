a858b4c926ec78bb5ed73887a6775e33fd5df49b
Tom Milligan <tom@tommilligan.net>
2017-08-12 22:27:03 +0100

    Added CORS headers

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var expressWinston = require('express-winston');
 var winston = require('winston');
+var cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(generalErrorHandler);
