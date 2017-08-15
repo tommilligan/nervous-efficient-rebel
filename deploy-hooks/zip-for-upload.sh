d5b12b1769755ec9449b2cc0b373e2ac2d8fa52b
Tom Milligan <tom@tommilligan.net>
2017-08-15 19:33:54 +0100

    Integrated deploy wrapping into travis.yml

--- deploy-hooks/zip-for-upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Zip files into archive
-git archive -o nervous-efficient-rebel.zip HEAD
-# Make deployment folder
-mkdir deploy
-# Move zip into deployment folder
-mv nervous-efficient-rebel.zip deploy/
